0e8d55327eb5b8ae1bc5ebf46bf214097b7c2b1f
Zeliha Gencel <zelihagencel@gmail.com>
2020-01-26 13:50:51 -0600

    displays table on console

--- db/db_api.js
+++ db/db_api.js
@@ -1,17 +1,73 @@
 const connection = require("./connection")
+const consoleTable = require('console.table');
+
+
+var main_questions = [{
+
+    type: "list",
+    message: "What would you like to do?",
+    name: "main",
+    choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Add Employee Manager",
+    ]
+}]
+
+var add_employee = [{
+    type: "input",
+    message: "What is the first name of the employee?",
+    name: "firstname",
+},
+{
+    type: "input",
+    message: "What is the last name of the employee?",
+    name: "lastname"
+},
+{
+    type: "input",
+    message: "What is the role of employee?",
+    name: "role"
+},
+{
+    type: "input",
+    message: "Who is the manager of employee?",
+    name: "manager"
+}]
 
 
 
 function display_all_employees() {
-    console.log("display_all was called");
-    var query = "SELECT * FROM employee";
+    // console.log("display_all was called");
+
+    var query = `SELECT employee.id, employee.first_name,employee.last_name,role.title, department.name,role.salary
+    FROM employee, department,role 
+    WHERE role.department_id = department.id
+    AND employee.role_id = role.id`
     connection.query(query, function (err, res) {
         if (err) throw err;
-        console.log(res);
+
+        let table_all = consoleTable.getTable(res)
+        console.log(table_all);
+
+        // for (let i = 0; i < res.length; i++) {
+        //     console.log(`At ${i} : ${res[i].first_name} ${res[i].last_name}`)
+        // }
     });
+
 }
 
 
+function display_all_employees_by_department() {
+    var query = "SELECT * FROM department";
 
+}
 
 module.exports.display_all_employees = display_all_employees;
+module.exports.display_all_employees_by_department = display_all_employees_by_department;
+module.exports.main_questions = main_questions;
+module.exports.add_employee = add_employee;
--- index.js
+++ index.js
@@ -1,65 +1,24 @@
 const inquirer = require("Inquirer");
 const alldb = require("./db/db_api");
+// const consoleTable = require('console.table');
 
 
-var main_questions = [{
+// const table = consoleTable.getTable([
+//     {
+//         name: 'zeliha',
+//         age: 35
+//     }, {
+//         name: 'ayse',
+//         age: 60
+//     }
+// ]);
 
-    type: "list",
-    message: "What would you like to do?",
-    name: "main",
-    choices: [
-        "View All Employees",
-        "View All Employees by Department",
-        "View All Employees by Manager",
-        "Add Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Add Employee Manager",
-    ]
-}]
-
-
-var add_employee = [{
-    type: "input",
-    message: "What is the first name of the employee?",
-    name: "firstname",
-},
-{
-    type: "input",
-    message: "What is the last name of the employee?",
-    name: "lastname"
-},
-{
-    type: "input",
-    message: "What is the role of employee?",
-    name: "role"
-},
-{
-    type: "input",
-    message: "Who is the manager of employee?",
-    name: "manager"
-}]
-
-
-// var update_employee = [{
-//     type: "list",
-//     message: "Who do you want to update?",
-//     name: "listEmployee",
-//     choices: [
-//         "View All Employees",
-//         "View All Employees by Department",
-//         "View All Employees by Manager",
-//         "Add Employee",
-//         "Remove Employee",
-//         "Update Employee Role",
-//         "Add Employee Manager",
-//     ]
-// }]
+// console.log(table);
 
 runAll();
 
 function runAll() {
-    inquirer.prompt(main_questions).then(function (answer) {
+    inquirer.prompt(alldb.main_questions).then(function (answer) {
         // console.log(answer);
         // console.log("--> action " + answer.main);
         switch (answer.main) {
