e62689b0f239f8d7b58aa450d4d400a8e142b9bd
Zeliha Gencel <zelihagencel@gmail.com>
2020-01-25 18:57:18 -0600

    connection is done

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+// var express = require('express');
+var mysql = require('mysql');
+// var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'rootroot',
+    database: 'employee_tracker'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+});
